f24e070a4302418f7dd6643bb04b6c9c5176e844
Mayk Gomes <maykcontato98@hotmail.com>
2025-10-31 18:22:38 -0300

    Forma de login e condição dashboard criada

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,40 @@
 import styles from './styles.module.css'
+import { useSession, signIn, signOut } from 'next-auth/react'
 import Link from 'next/link'
+import Nextauth from '@/pages/api/auth/[...nextauth]'
 
 
 export default function Header() {
+
+
+    const { data: session, status } = useSession();
     return (
         <header className={styles.header}>
             <section className={styles.content}>
                 <nav className={styles.nav}>
                     <Link href="/">
-                    <h1 className={styles.logo}>
-                    Tarefas<span>+</span>
-                    </h1>
+                        <h1 className={styles.logo}>
+                            Tarefas<span>+</span>
+                        </h1>
                     </Link>
-                    <Link href="/dashboard" className={styles.link}>
-                        Meu Painel
-                    </Link>
+                    {session?.user && (
+                        <Link href="/dashboard" className={styles.link}>
+                            Meu Painel
+                        </Link>
+                    )}
                 </nav>
-                <button className={styles.loginButton}>
-                    Acessar
-                </button>
+                {status === 'loading' ? (
+                    <>
+                    </>
+                ) : session ? (
+                    <button className={styles.loginButton} onClick={() => signOut()}>
+                        Olá, {session.user?.name}
+                    </button>
+                ) : (
+                    <button className={styles.loginButton} onClick={() => signIn("google")}>
+                        Acessar
+                    </button>
+                )}
             </section>
         </header>
     )
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,15 @@
+import { GetServerSideProps } from 'next'
+import React from 'react'
 import styles from './styles.module.css'
 import Head from 'next/head'
+import { getSession } from 'next-auth/react'
+
 
 
 export default function Dashboard() {
+
+
+
     return (
         <div className={styles.container}>
            <Head>
@@ -13,3 +20,22 @@ export default function Dashboard() {
         </div>
     )
 }
+
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const session = await getSession({req})
+
+    if(!session?.user){
+        // se não tem usuário vamos redirecionar para home
+        return {
+            redirect: {
+                destination: '/',
+                permanent:false
+            }
+        }
+    }
+    return {
+        props: {}
+    }
+};
